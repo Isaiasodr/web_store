1ace8ec2d0d80f825fc9e9ee3ecc509bea0497f3
unknown <isaiasodr@gmail.com>
2022-05-09 20:08:37 -0300

    finalização da pagina de cadastro e login, início da página do carinho de compras

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@
 import Home from "./pages/Home";
 import Product from "./pages/product";
 import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/login";
+import Cart from "./pages/Cart";
 
 const App = () => {
   
-  return <Product/>
+  return <Cart/>
 };
 
 export default App;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,72 @@
+
+import styled from "styled-components"
+
+
+const Container = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background-color: #F0D2D7;
+    display: flex;
+    align-items:center;
+    justify-content: center;
+`
+
+const Wrapper = styled.div`
+    width:40%;
+    padding: 20px;
+    background-color: white;
+
+`
+const Title = styled.h1`
+    font-size: 24px;
+    font-weight: 30;
+`
+const Form = styled.form`
+    display: flex;
+    flex-wrap: wrap;
+
+`
+const Input = styled.input`
+
+        flex: 1;
+        min-width: 40%;
+        margin: 20px 10px 0px 0px;
+        padding: 10px;
+`
+const Agreement = styled.p`
+        font-size: 12px;
+        margin: 20px 0px;
+`
+const Button = styled.button`
+    width: 40%;
+    border: none;
+    padding: 15px 20px;
+    background-color: teal;
+    color: white;
+    cursor: pointer;
+
+`
+
+const Register = () => {
+    return (
+        <Container>
+            <Wrapper>
+                <Title>Criar conta</Title>
+                <Form>
+                    <Input placeholder="nome" />
+                    <Input placeholder="Sobrenome" />
+                    <Input placeholder="E-mail" />
+                    <Input placeholder="Usuário" />
+                    <Input placeholder="Senha" />
+                    <Input placeholder="Confirmar Senha" />
+                    <Agreement>
+                        Ao criar uma conta, você está de acordo com os termos. <b>POLÍTICA DE PRIVACIDADE</b>
+                    </Agreement>  
+                </Form>
+                <Button>Cadastrar</Button>
+            </Wrapper>
+        </Container>
+    )
+}
+
+export default Register
