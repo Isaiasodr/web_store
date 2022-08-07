05b24ac46a5209d52fee2682882c865367556bb2
Isaías Oliveira dos Reis <isaiasodr@gmail.com>
2022-08-07 12:57:55 -0300

    criação das rotas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,22 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/login";
 import Cart from "./pages/Cart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Register />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<ProductList />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
